2325c74bfb4bc0fc020247ce9cb0b961337fb2fc
david anderson <d.a.m.anderson@gmail.com>
2016-09-28 12:50:30 +0100

    SRP practice

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,21 @@ class DockingStation
 	end
 
 	def release_bike
-	 fail 'No bikes available' if @bikes.empty?
+	 fail 'No bikes available' if empty?
 	 @bikes.pop
 	end
 
 	def dock(orange_bike)
-		fail 'Docking station full' if @bikes.count >= 20
+		fail 'Docking station full' if full?
 		@bikes << orange_bike
+	end
+
+private
+	def full?
+		@bikes.count >= 20
+	end
+
+	def empty?
+		@bikes.empty?
 	end
 end
