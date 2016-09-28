57d26052ceb8c205a4c89f5a3fe8b9bf6bd3690d
david anderson <d.a.m.anderson@gmail.com>
2016-09-28 11:06:13 +0100

    bike and docking methods tested

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,16 @@ it "produces working bikes" do
   red = subject.release_bike
   expect(red).to be_working
   end
+
+it "docking a bike returns the bike" do
+  green_bike = Bike.new
+  expect(subject.dock(green_bike)).to eq green_bike
 end
 
+it "bike method returns the bike we docked" do
+  yellow_bike = Bike.new
+  subject.dock(yellow_bike)
+  expect(subject.bike).to eq yellow_bike
+end
+
+end
